0ebdf42dfd88b6889fafc7423de111ec34ee53a2
jeantsai <jeantsai@qq.com>
2023-07-21 08:58:34 +0800

    2 files modified

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 console.log('Hello!');
+
+class TodoItem extends HTMLElement {
+  constructor () {
+    super()
+    this.innerHTML = "My first web component"
+  }
+}
+
+customElements.define("todo-item", TodoItem)
